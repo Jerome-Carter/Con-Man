30b8d10000c8ef1e08331ab0a76c38872bd4f020
James Carter <jeromecarter0722@gmail.com>
2017-03-15 18:17:50 -0400

    Initialize Repo

---
+++ Con Man/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Con Man
+//
+//  Created by James Carter on 3/15/17.
+//  Copyright © 2017 James Carter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
