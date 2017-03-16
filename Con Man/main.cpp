c43bde01985a9a13ce70dfa6710add35bc4fc85b
James Carter <jeromecarter0722@gmail.com>
2017-03-15 21:13:16 -0400

    Adds Socket interface

--- Con Man/main.cpp
+++ Con Man/main.cpp
@@ -6,11 +6,16 @@
 //  Copyright © 2017 James Carter. All rights reserved.
 //
 
+#include "udp.h"
 #include "easylogging++.h"
 
 INITIALIZE_EASYLOGGINGPP
 
+using namespace Con_Man;
+
 int main(int argc, const char * argv[]) {
     LOG(INFO) << "Con Man started!";
+    Con_Man::UDP udp_socket("0.0.0.0", 0);
+    udp_socket.open();
     return 0;
 }
---
+++ Con Man/socket.h
@@ -0,0 +1,27 @@
+//
+//  socket.h
+//  Con Man
+//
+//  Created by James Carter on 3/15/17.
+//  Copyright © 2017 James Carter. All rights reserved.
+//
+
+#pragma once
+
+#include <functional>
+
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+namespace Con_Man {
+    class Socket {
+    public:
+        virtual bool open() = 0;
+        virtual void close() = 0;
+        virtual void send(const char* data) const = 0;
+        virtual void receive(const std::function<void(char*)>& call) const = 0;
+    };
+}
